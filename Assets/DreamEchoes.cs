a53bdd9279f1f0c39b1beb618735983ddadb00e3
Daniel Rainy <danirainy@outlook.com>
2024-05-05 14:16:36 -0700

    Initial setup

---
+++ Assets/DreamEchoes.cs
@@ -0,0 +1,16 @@
+using WeaverCore;
+
+public class DreamEchoes : WeaverMod
+{
+    public DreamEchoes() : base("DreamEchoes") { }
+
+    public override string GetVersion()
+    {
+        return "1.0";
+    }
+
+    public override void Initialize()
+    {
+        
+    }
+}
