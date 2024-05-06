68062cdbe4c83b4cbc8c2b79a87e7558cbff5b11
Daniel Rainy <danirainy@outlook.com>
2024-05-05 17:14:02 -0700

    Refactor

---
+++ Core/DreamEchoesCore/Imports.cs
@@ -0,0 +1,4 @@
+﻿global using Modding;
+global using System.Collections;
+global using UnityEngine;
+global using WeaverCore.Assets.Components;
---
+++ Unity/Assets/DreamEchoes.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using WeaverCore;
+using UnityEngine;
+
+namespace DreamEchoes
+{
+    public class DreamEchoes : WeaverMod
+    {
+        public DreamEchoes() : base("DreamEchoes") { }
+
+        public override string GetVersion() => "1.0.0.0";
+    }
+}
