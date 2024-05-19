b81dbae1025bfe32d4088f435d73b7a4725fb643
Daniel Rainy <danirainy@outlook.com>
2024-05-18 19:16:36 -0700

    don't allow dash towards for parry

--- Core/DreamEchoesCore/Entities/Enemies/Seer/SeerStateMachine/States/Parry.cs
+++ Core/DreamEchoesCore/Entities/Enemies/Seer/SeerStateMachine/States/Parry.cs
@@ -6,6 +6,58 @@ namespace DreamEchoesCore.Entities.Enemies.Seer.SeerStateMachine;
 
 internal partial class SeerStateMachine : EntityStateMachine
 {
+    private IEnumerator<Transition> ParrySwapSlash()
+    {
+        if (!FacingTarget())
+        {
+            yield return new CoroutineTransition
+            {
+                Routine = Turn()
+            };
+        }
+        var velocityX = (Target().Position().x - Position.x);
+        velocityX *= Config.SlashVelocityXScale;
+        var minVelocityX = Config.ControlledSlashVelocityX;
+        if (Mathf.Abs(velocityX) < minVelocityX)
+        {
+            velocityX = Mathf.Sign(velocityX) * minVelocityX;
+        }
+        Velocity = Vector2.zero;
+
+        IEnumerator<Transition> Slash(string slash)
+        {
+            if (!FacingTarget())
+            {
+                velocityX *= -1;
+                Velocity *= -1;
+                yield return new CoroutineTransition
+                {
+                    Routine = Turn()
+                };
+            }
+            var previousVelocityX = Velocity.x;
+            Transition updater(float normalizedTime)
+            {
+                var currentVelocityX = Mathf.Lerp(previousVelocityX, velocityX, normalizedTime);
+                Velocity = new Vector2(currentVelocityX, 0);
+                return new NoTransition();
+            }
+            yield return new CoroutineTransition
+            {
+                Routine = animator.PlayAnimation(slash, updater)
+            };
+        }
+        foreach (var slash in new string[] { "Slash1", "Slash2" })
+        {
+            yield return new CoroutineTransition
+            {
+                Routine = Slash(slash)
+            };
+        }
+
+        Velocity = Vector2.zero;
+    }
+
     [State]
     private IEnumerator<Transition> Parry()
     {
@@ -46,9 +98,15 @@ internal partial class SeerStateMachine : EntityStateMachine
             {
                 yield return new CoroutineTransition { Routine = Turn() };
             }
+            var swapped = false;
             var velocityX = Config.ParryVelocityX * Direction();
             Transition updater(float normalizedTime)
             {
+                if (!FacingTarget())
+                {
+                    swapped = true;
+                    return null;
+                }
                 var currentVelocityX = Mathf.Lerp(0, velocityX, normalizedTime);
                 Velocity = new Vector2(currentVelocityX, 0);
                 return new NoTransition();
@@ -57,11 +115,22 @@ internal partial class SeerStateMachine : EntityStateMachine
             {
                 Routine = animator.PlayAnimation("ParryCounter", updater)
             };
-            Velocity = Vector2.zero;
-            yield return new CoroutineTransition
+            if (swapped)
             {
-                Routine = animator.PlayAnimation("ParryEnd")
-            };
+                Reset();
+                yield return new CoroutineTransition
+                {
+                    Routine = ParrySwapSlash()
+                };
+            }
+            else
+            {
+                Velocity = Vector2.zero;
+                yield return new CoroutineTransition
+                {
+                    Routine = animator.PlayAnimation("ParryEnd")
+                };
+            }
         }
         yield return new ToState { State = nameof(Idle) };
     }
