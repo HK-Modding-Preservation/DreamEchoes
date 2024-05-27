c7b9f797285a1b694dcbc6491eb018004fa00f16
Daniel Rainy <danirainy@outlook.com>
2024-05-27 12:59:36 -0700

    Fix hp bar

---
+++ Core/DreamEchoesCore/EnemyHPBar.cs
@@ -0,0 +1,31 @@
+﻿// Credit to https://github.com/ygsbzr/Enemy-HP-Bars
+
+using MonoMod.ModInterop;
+using System;
+using UnityEngine;
+
+namespace DreamEchoesCore;
+
+internal static class EnemyHPBar
+{
+    [ModImportName(nameof(EnemyHPBar))]
+    private static class EnemyHPBarImport
+    {
+#pragma warning disable 649
+        public static Action<GameObject> MarkAsBoss;
+        public static Action<GameObject> RefreshHPBar;
+#pragma warning restore 649
+    }
+
+    static EnemyHPBar() => typeof(EnemyHPBarImport).ModInterop();
+
+    public static void MarkAsBoss(this GameObject go)
+    {
+        EnemyHPBarImport.MarkAsBoss?.Invoke(go);
+    }
+
+    public static void RefreshHPBar(this GameObject go)
+    {
+        EnemyHPBarImport.RefreshHPBar?.Invoke(go);
+    }
+}
