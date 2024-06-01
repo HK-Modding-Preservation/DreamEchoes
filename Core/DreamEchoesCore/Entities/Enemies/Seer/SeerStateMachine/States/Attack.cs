4a4b13dcf365f85267503133d38a33d62dc2b24e
Daniel Rainy <danirainy@outlook.com>
2024-05-31 20:56:23 -0700

    fix

--- Core/DreamEchoesCore/Entities/Enemies/Seer/SeerStateMachine/States/Attack.cs
+++ Core/DreamEchoesCore/Entities/Enemies/Seer/SeerStateMachine/States/Attack.cs
@@ -8,8 +8,8 @@ internal partial class SeerStateMachine : EntityStateMachine
 {
     private RandomSelector<string> attackRandomSelector = new([
         new(value: nameof(Dash), weight: 1, maxCount: 2, maxMiss: 5),
-        //new(value: nameof(Slash), weight: 1, maxCount: 2, maxMiss: 5),
-        //new(value: nameof(Hug), weight: 1, maxCount: 2, maxMiss: 5),
+        new(value: nameof(Slash), weight: 1, maxCount: 2, maxMiss: 5),
+        new(value: nameof(Hug), weight: 1, maxCount: 2, maxMiss: 5),
         new(value: nameof(TeleSlash), weight: 1, maxCount: 2, maxMiss: 5),
     ]);
 
