03000ba0b0a5485eaedfc16b8cc76bad0513a2f5
Daniel Rainy <danirainy@outlook.com>
2024-06-10 12:14:25 -0700

    Tree refine

--- Bridge/DreamEchoesCore/Entities/Enemies/Seer/SeerStateMachine/TreeStateMachine.cs
+++ Bridge/DreamEchoesCore/Entities/Enemies/Seer/SeerStateMachine/TreeStateMachine.cs
@@ -6,5 +6,9 @@ internal class TreeStateMachine : MonoBehaviour
 {
     public GameObject Attacks;
 
+    public AudioClip GrowSound;
+
     public void StartAttacks() { }
+
+    public void PlayGrowSound() { }
 }
--- Core/DreamEchoesCore/Entities/Enemies/Seer/SeerStateMachine/TreeStateMachine.cs
+++ Core/DreamEchoesCore/Entities/Enemies/Seer/SeerStateMachine/TreeStateMachine.cs
@@ -8,6 +8,9 @@ internal class TreeStateMachine : StateMachine
 {
     public GameObject Attacks;
 
+    private AudioSource audioSource;
+    public AudioClip GrowSound;
+
     [State]
     private IEnumerator<Transition> Begin()
     {
@@ -22,11 +25,21 @@ internal class TreeStateMachine : StateMachine
         globalTransitions: [])
     { }
 
+    protected override void StateMachineStart()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
     public void StartAttacks()
     {
         var attackPos = Attacks.transform.position;
         attackPos.x = transform.parent.position.x;
         Attacks.transform.position = attackPos;
         Attacks.SetActive(true);
+    }
+
+    public void PlayGrowSound()
+    {
+        audioSource.PlayOneShot(GrowSound);
     }
 }
