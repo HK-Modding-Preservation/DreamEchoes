886055aaac0d2e209ec6116b9bdb8c6b0b25f1da
Daniel Rainy <danirainy@outlook.com>
2024-06-11 13:08:41 -0700

    Better ddark prevent

---
+++ Core/DreamEchoesCore/Utils/DDNot.cs
@@ -0,0 +1,25 @@
+﻿using GlobalEnums;
+using UnityEngine;
+
+namespace DreamEchoesCore.Utils;
+
+public class DDNot : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            return;
+        }
+        RingLib.Log.LogInfo("DDNot", "hit" + other.gameObject.name);
+        if (other.gameObject.name == "HeroBox")
+        {
+            var hero = HeroController.instance;
+            if (hero.damageMode == DamageMode.HAZARD_ONLY)
+            {
+                hero.damageMode = DamageMode.FULL_DAMAGE;
+                RingLib.Log.LogInfo("", "don't ddark");
+            }
+        }
+    }
+}
